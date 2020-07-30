f3df5578b394fb60d27225c54d19f250c67202ad
Paweł Mazur <pawelmazur835@gmail.com>
2020-07-30 17:22:44 +0200

    Dodanie funkcjonalnośći zapisywania adresu, dodanie javadocs

--- src/test/java/przyklad1/MyStoreRegister.java
+++ src/test/java/przyklad1/MyStoreRegister.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.Date;
 import java.util.Random;
@@ -14,7 +15,7 @@ public class MyStoreRegister {
 
     private WebDriver driver;
     private static final int NUMBER_OF_ACCOUNTS_TO_CREATE = 10;
-    private static final int RANDOM_MAX_RANGE = 1000;
+    private static final int RANDOM_MAX_RANGE = 100;
     Random random = new Random();
     Date date= new Date();
     String webpage = "https://prod-kurs.coderslab.pl/index.php?controller=authentication&create_account=1";
@@ -22,6 +23,12 @@ public class MyStoreRegister {
     String [] firstNames = {"Paweł", "Karol", "Maciej", "Jakub", "Michał", "Zbigniew", "Tester"};
     String [] lastNames = {"Kowalski", "Mazur", "Lewandowski", "Aleksandrowicz", "Danielak", "Testowy"};
     String [] emails = {"@gmail.com", "@yahoo.com", "@interia.pl", "@onet.pl", "@wp.pl", "@o2.pl", "@gazeta.pl"};
+    String [] companies = {"Shopify", "Google", "Facebook", "Twitter", "Apple", "Netflix", "Amazon", "Tesla", "Microsoft"};
+    String [] addresses = {"York Road", "Mill Lane", "Chester Road", "Park Lane", "Highfield Road", "Station Road", "Broadway",
+    "York Road"};
+    String [] cities = {"INVERNESS", "DURHAM", "READING", "BELFAST", "DARTFORD", "SWINDON", "DONCASTER", "NOTTINGHAM",
+    "MANCHESTER", "CHESTER"};
+
 
 
     @Before
@@ -38,24 +45,44 @@ public class MyStoreRegister {
     }
 
     @Test
-    public void MyStoreRegisterTest() throws InterruptedException {
+    public void MyStoreRegisterAndAccountSetingsTest() throws InterruptedException {
 
         //Create accounts based on NUMBER_OF_ACCOUNTS_TO_CREATE
         for (int i = 0; i < NUMBER_OF_ACCOUNTS_TO_CREATE; i++) {
 
-            //Finding elements
+            //Finding registration form elements
             WebElement firstName = driver.findElement(By.name("firstname"));
             WebElement lastName = driver.findElement(By.name("lastname"));
             WebElement email = driver.findElement(By.name("email"));
             WebElement password = driver.findElement(By.name("password"));
-            WebElement saveBtn = driver.findElement(By.xpath("//button[@data-link-action='save-customer']"));
+            WebElement RegisterFormSaveBtn = driver.findElement(By.xpath("//button[@data-link-action='save-customer']"));
 
-            //Enter data
+            //Enter data to registration form
             firstName.sendKeys(generateRandomFirstName());
             lastName.sendKeys(generateRandomLastName());
             email.sendKeys(generateRandomEmail());
             password.sendKeys(randomPasswordGenerator());
-            saveBtn.click();
+            RegisterFormSaveBtn.click();
+
+            //Going to Your account / Addresses
+            driver.get("https://prod-kurs.coderslab.pl/index.php?controller=address");
+
+            //Finding elements on Address form
+            WebElement company = driver.findElement(By.name("company"));
+            WebElement addres = driver.findElement(By.name("address1"));
+            WebElement postcode = driver.findElement(By.name("postcode"));
+            WebElement city = driver.findElement(By.name("city"));
+            Select countryDropdown = new Select (driver.findElement(By.name("id_country")));
+            WebElement addressFormSaveBtn = driver.findElement(By.cssSelector("button.btn.btn-primary.float-xs-right"));
+
+            //Entering data to Address form
+            company.sendKeys(generateRandomCompany());
+            addres.sendKeys(generateRandomAddress());
+            postcode.sendKeys(generateRandomPostcode());
+            city.sendKeys(generateRandomCity());
+            countryDropdown.selectByIndex(1);
+            addressFormSaveBtn.click();
+
 
             //Log out
             WebElement logOutBtn = driver.findElement(By.xpath("//a[@class='logout hidden-sm-down']"));
@@ -64,24 +91,95 @@ public class MyStoreRegister {
         }
     }
 
-    //generate random email with the use of timestamp
+    /**
+     * Generates the random email
+     * Uses the emails table and timestamp to randomize the output
+     * @return random generate email
+     */
     public String generateRandomEmail(){
         long time = date.getTime();
         return "testowy" + time + random.nextInt(RANDOM_MAX_RANGE) +
                 emails[random.nextInt(RANDOM_MAX_RANGE) % emails.length];
     }
 
+    /**
+     * Generates random first name
+     * Uses the firstNames table
+     * @return random firstName
+     */
     public String generateRandomFirstName(){
         return firstNames[random.nextInt(RANDOM_MAX_RANGE) % firstNames.length];
     }
 
+    /**
+     * Generates random last name
+     * Uses the lastNames table
+     * @return random lastName
+     */
     public String generateRandomLastName(){
         return lastNames[random.nextInt(RANDOM_MAX_RANGE) % lastNames.length];
     }
 
-    //generate random 5 character password
+    /**
+     * Generates random company
+     * Uses the companies table
+     * @return random company
+     */
+    public String generateRandomCompany(){return companies[random.nextInt(RANDOM_MAX_RANGE) % companies.length];}
+
+    /**
+     * Generates random address
+     * Uses random number and addresses table
+     * @return random address
+     */
+    public String generateRandomAddress(){
+        return random.nextInt(RANDOM_MAX_RANGE) + " " +addresses[random.nextInt(RANDOM_MAX_RANGE) % addresses.length];
+    }
+
+    /**
+     * Generates random UK postal code
+     * Creates the code with the following format '[A-Z][A-Z][0-9][0-9] [0-9][A-Z][A-Z]'
+     * @return random postcode
+     */
+    public String generateRandomPostcode(){
+        int numberOfCharsinCode = 8;
+        StringBuilder sb = new StringBuilder();
+        String setOfCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String setOfNumbers = "0123456789";
+        int k = 0;
+
+        for (int i = 0; i < numberOfCharsinCode; i++) {
+
+            if (i == 2 || i == 3 || i == 5){
+                k = random.nextInt(setOfNumbers.length() - 1);
+                sb.append(setOfNumbers.charAt(k));
+            }
+            else if (i == 4){
+                sb.append(" ");
+            }
+            else {
+                k = random.nextInt(setOfCharacters.length() - 1);
+                sb.append(setOfCharacters.charAt(k));
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Generates random city
+     * Uses the cities table
+     * @return random city
+     */
+    public String generateRandomCity(){
+        return cities[random.nextInt(RANDOM_MAX_RANGE) % cities.length];
+    }
+
+    /**
+     * Generates random 5 character password
+     * Uses lowercase characters, digital numbers and special characters
+     * @return random password
+     */
     public String randomPasswordGenerator(){
-        String result;
         StringBuilder sb = new StringBuilder();
         int numberOfCharsInPwd = 5;
         String setOfCharacters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
@@ -90,6 +188,6 @@ public class MyStoreRegister {
             int k = random.nextInt(setOfCharacters.length()-1);
             sb.append(setOfCharacters.charAt(k));
         }
-        return result = sb.toString();
+        return sb.toString();
     }
 }
